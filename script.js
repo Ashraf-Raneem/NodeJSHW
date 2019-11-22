add78bd21681bf75ce0b08617d4804ba4ffb5586
Ashraf-Raneem <48803665+Ashraf-Raneem@users.noreply.github.com>
2019-11-23 03:04:16 +0600

    L11-L13,GET and POST

---
+++ script.js
@@ -0,0 +1,37 @@
+/* Add your script here */
+//task 1
+$("#list").click(function(){
+    $.ajax({
+        method: 'GET',
+        url: 'http://skeleton.mernbook.com/api/users'
+       }).done(function(response){
+        response.forEach(element =>{
+            let user = $('<p>').text(element.name);
+            $('#users').append(user)
+        });
+       }).fail(function(response){
+        console.log(response);
+       });
+       
+})
+
+//Task 2
+$('#add').click(function(){
+    let user = {name:'',email: ' ', password: ''}
+    user.name = $('#name').val(); 
+    user.email = $('#email').val(); 
+    user.password = $("#password").val(); 
+    $.ajax({
+        method: 'POST',
+        url: 'http://skeleton.mernbook.com/api/users',
+        data: user
+       }).done(function(response){
+        $('#name').val(''); 
+        $('#email').val(''); 
+        $("#password").val('');
+        $('#error').text('The client has been added');
+        }).fail(function(response){
+            $("#error").text(response.responseJSON.error)
+        console.log(response);
+       });
+})
